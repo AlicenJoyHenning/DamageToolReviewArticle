d0bb2368c94c2d624cd3b91d573868a2fbe6e780
Alicen Henning <alicen.jhb@gmail.com>
2024-10-19 19:16:16 +0200

    Finalise ensembleKQC script

--- code/B - Testing strategies on processed data/2.2 - Run EnsembleKQC.sh
+++ code/B - Testing strategies on processed data/2.2 - Run EnsembleKQC.sh
@@ -28,3 +28,28 @@ for input_file in "$input_dir"*.csv; do
   # Run the second Python script
   python ./runEnsembleKQC.py --input_path="$intermediate_file" --labeled=false --output_path="$output_file"
 done
+
+
+# Define directories
+input_dir="/Users/alicen/Projects/ReviewArticle/input/mouse"
+intermediate_dir="/Users/alicen/Projects/ReviewArticle/EnsembleKQC_input/"
+output_dir="/Users/alicen/Projects/ReviewArticle/EnsembleKQC_output/"
+
+# Loop through all .csv files in the input directory
+for input_file in "$input_dir"*.csv; do
+
+  # Extract the base name of the file 
+  base_name=$(basename "$input_file" | cut -d'_' -f1)
+  
+  # Define the intermediate file name
+  intermediate_file="${intermediate_dir}${base_name}_input.csv"
+  
+  # Run the first Python script
+  python ./extractFeatures.py "$input_file" "$intermediate_file" mouse --normalize=false
+  
+  # Define the output file name
+  output_file="${output_dir}${base_name}_input.csv"
+  
+  # Run the second Python script
+  python ./runEnsembleKQC.py --input_path="$intermediate_file" --labeled=false --output_path="$output_file"
+done
