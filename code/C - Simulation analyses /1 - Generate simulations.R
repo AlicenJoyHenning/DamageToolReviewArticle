150fc93dc27bef203ba1ee9b35304c133b40ee99
AlicenJoyHenning <alicen.jhb@gmail.com>
2024-10-23 11:29:33 +0200

    Simulation study script preparation

--- code/C - Simulation analyses /1 - Generate simulations.R
+++ code/C - Simulation analyses /1 - Generate simulations.R
@@ -1,5 +1,27 @@
 # SCRIPT CONTEXT 
 #
-# Simulate datasets with set number of cell types, known set of DEGs. 
-# In
+# Simulate datasets with set number of cell types and known set of DEGs.
+# Importantly, be able to add simulated damaged cells to the datasets: 
+#   Case 1: 0 - 5 % damage
+#   Case 2: 5 - 10 % damage
+#   Case 3: 10 - 15 % damage
+#   Case 4: 15 - 20 % damage
+#   Case 5: 20 - 30 % damage
 #
+# Simulation tools: 
+#   Splatter: https://bioconductor.org/packages/devel/bioc/vignettes/splatter/inst/doc/splatter.html#Session_information
+#         - don't know about DEGs or how to add damage, also sce not seurat 
+# 
+
+
+#-------------------------------------------------------------------------------
+# PREPARATIONS
+#-------------------------------------------------------------------------------
+
+library(devtools)
+
+
+
+
+
+
